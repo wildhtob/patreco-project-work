6be5ac3aa026a47b52e082c55618ea3f9eedfbe5
wildhtob <tpwildhaber@gmail.com>
2021-06-11 16:04:36 +0200

    further data exploration

--- scripts/analysis.r
+++ scripts/analysis.r
@@ -1,5 +1,8 @@
-
 # libraries ---------------------------------------------------------------
+
+# install package with wildobar data:
+# devtools::install_github("ComputationalMovementAnalysis/ComputationalMovementAnalysisData")
+
 
 library(tidyverse) # tidy essentials (ggplot, purr, tidyr, readr, dplyr)
 library(lubridate) # handling dates and time
@@ -34,16 +37,18 @@ wildboar_sf <-st_as_sf(wildboar_raw, coords = c("E", "N"), crs = 2056)
 
 # data exploration --------------------------------------------------------
 
-# plot animals
-# ggplot(wildboar_raw, aes(E, N, colour = TierName)) +
-#  geom_point() +
+# plotting data points ####
 
-wildboar_sf %>% 
-  ggplot(aes(fill = TierName)) +
+ggplot(wildboar_sf, aes(color = TierName)) +
   geom_sf(alpha = 0.4) +
-  coord_sf(datum = 2056) 
+  coord_sf(datum = 2056)
 
-# sampling regime
+ggplot(wildboar_sf, aes(color = as.factor(TierID))) +
+  geom_sf(alpha = 0.4) +
+  coord_sf(datum = 2056) +
+  scale_color_discrete(name = "AnimalID")
+
+# sampling regime ####
 
 limits <- c(0,200)
 breaks = seq(0,200,50)
@@ -62,7 +67,7 @@ wildboar_raw %>%
   theme(legend.position = "top") +
   guides(color = guide_colorbar(title.position = "top", title.hjust = .5, barwidth = unit(20, "lines"), barheight = unit(.5, "lines")))
 
-# temporal overlap
+# temporal overlap ####
 
 ?wildschwein_overlap_temp
 
@@ -82,9 +87,15 @@ wildboar_overlap %>%
   geom_errorbarh() +
   facet_grid(Groups~., scales = "free_y", space = "free_y")
 
-# spatial overlap
+ggplot(wildboar_sf, aes(x = DatetimeUTC, y = TierName)) +
+  geom_point() +
+  scale_x_datetime(breaks = "1 month") +
+  theme_grey()
 
-# convex hull
+
+# spatial overlap ####
+
+# convex hull ####
 
 wildboar_sf <- wildboar_sf %>%
   mutate(tiercollar = paste(TierID, TierName, CollarID)) 
@@ -94,7 +105,7 @@ mcp <- wildboar_sf %>%
   summarise() %>%
   st_convex_hull()
 
-# plot convex hull
+# plot convex hull ####
 
 mcp %>%
   mutate(tiercollar = paste(TierID, TierName, CollarID)) %>%
@@ -109,4 +120,66 @@ tmap_mode("view") +
   tm_fill("TierName", alpha = 0.5) +
   tm_borders(col = "red", lwd = 1) +
   tm_layout(legend.bg.color = "white")
+
+
+
+# time lags ####
+
+wildboar_lags <- wildboar_raw %>%
+  group_by(TierID) %>% 
+  mutate(timelag = as.integer(difftime(lead(DatetimeUTC), DatetimeUTC), units = "secs"))
+
+# round lags on -2 digits 
+wildboar_lags$timelag_rounded <- round(wildboar_lags$timelag, -2)
+
+# build categories with rounded lags
+wildboar_lag_cat <- wildboar_lags %>%
+#  st_drop_geometry() %>%
+  group_by(timelag_rounded) %>%
+  summarise(count = n())
+
+
+# viualise change in lag over time ####
+wildboar_lags %>%
+  filter(timelag < 30000 & timelag > 0) %>%
+  ggplot(., aes(DatetimeUTC, timelag, col = TierName)) +
+  geom_point() +
+  geom_line(size = 0.1)
+
+# calculating steplength ####
+
+wildboar_lags$steplength <- wildboar_raw %>%
+  {
+    (.$E - lead(.$E))^2 + (.$N - lead(.$N))^2
+  } %>%
+  sqrt()
+
+# calculating speed based on timelag (t) in secs and steplength (s) in meter ####
+
+wildboar_lags$speed <- wildboar_raw %>% {
+  .$steplength / .$timelag
+}
+
+# cross-scale movement analysis ####
+
+seq3 <- seq(from = 1, to = nrow(wildboar_raw), by = 3)
+seq6 <- seq(from = 1, to = nrow(wildboar_raw), by = 6)
+seq9 <- seq(from = 1, to = nrow(wildboar_raw), by = 9)
+
+wildboar_3 <- wildboar_lags %>% slice(seq3)
+wildboar_6 <- wildboar_lags %>% slice(seq6)
+wildboar_9 <- wildboar_lags %>% slice(seq9)
+
+# sample code for different intervals (to be discussed)
+
+wildboar_3$steplength <- wildboar_3 %>%
+  {
+    (.$E - lead(.$E))^2 + (.$N - lead(.$N))^2
+  } %>%
+  sqrt()
+
+wildboar_3$speed <- wildboar_3 %>% {
+  .$steplength / .$timelag
+}
+
 
